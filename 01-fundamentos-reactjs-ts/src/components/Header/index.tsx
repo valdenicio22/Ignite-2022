e25c100292289c12eb890253a813fc3dbd69d252
Valdenício Ferreira <valdenicio22@gmail.com>
2022-06-06 19:34:51 -0300

    feat: create Header component

--- 01-fundamentos-reactjs-ts/src/components/Header/index.tsx
+++ 01-fundamentos-reactjs-ts/src/components/Header/index.tsx
@@ -1,5 +1,17 @@
+import RocketIcon from '../../assets/rocket.svg'
+
 const Header = () => {
-  return <div className="bg-black">Xon Xin</div>
+  return (
+    <header className="bg-gray-700 w-full h-52 flex items-center justify-center">
+      <div className="flex items-center justify-center gap-3">
+        <img src={RocketIcon} alt="rocket icon" />
+        <p>
+          <span className="text-4xl text-[#4EA8DE] ">to</span>
+          <span className="text-4xl text-[#5E60CE] ">do</span>
+        </p>
+      </div>
+    </header>
+  )
 }
 
 export default Header
