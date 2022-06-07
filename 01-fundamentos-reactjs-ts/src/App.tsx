89bf94da3e2051c0867cd9ee3f918cb872da8c7c
Valdenício Ferreira <valdenicio22@gmail.com>
2022-06-06 21:23:29 -0300

    feat: import Header component

--- 01-fundamentos-reactjs-ts/src/App.tsx
+++ 01-fundamentos-reactjs-ts/src/App.tsx
@@ -1,9 +1,14 @@
-import Header from './components/Header'
+import { Header } from './components/Header'
+import { Input } from './components/Input'
 
 function App() {
   return (
     <div className="bg-slate-400">
       <Header />
+      <div className="flex items-center gap-4 max-w-3xl mx-auto ">
+        <Input />
+        <button>Add</button>
+      </div>
     </div>
   )
 }
