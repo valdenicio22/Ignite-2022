ddfc04552c254145848a852f5e0a12ab762bfb5a
Valdenício Ferreira <valdenicio22@gmail.com>
2022-06-08 09:07:28 -0300

    feat: add fn to add task to list

--- 01-fundamentos-reactjs-ts/src/App.tsx
+++ 01-fundamentos-reactjs-ts/src/App.tsx
@@ -5,15 +5,19 @@ import { Input } from './components/Input'
 import { AiOutlinePlusCircle, AiOutlineEdit } from 'react-icons/ai'
 import { HiOutlineTrash } from 'react-icons/hi'
 import { Todo } from './types/types'
+import { api } from './service/api'
 
 function App() {
   const [todos, setTodos] = useState<Todo[]>([])
+  const [todo, setTodo] = useState('')
+  const [completedCount, setCompletedCount] = useState(0)
 
   useEffect(() => {
     try {
       fetch('http://localhost:3004/todos')
         .then((response) => response.json())
         .then((data) => setTodos(data))
+      setCompletedCount(completedCount)
     } catch (error) {
       console.log(error)
     }
@@ -21,9 +25,8 @@ function App() {
 
   const updateTodos = async (updatedTodos: Todo[]) => {
     try {
-      await fetch('http://localhost:3004/todos', {
-        method: 'POST',
-        body: JSON.stringify(updatedTodos)
+      await api.put('/todos', {
+        ...updatedTodos
       })
       setTodos(updatedTodos)
     } catch (error) {
@@ -41,19 +44,48 @@ function App() {
     updateTodos(updatedTodos)
   }
 
-  const handleSubmitNewTask = (e: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmitNewTask = async (e: FormEvent) => {
     e.preventDefault()
+
+    if (!todo) return
+    const updatedTodo = {
+      id: Math.round(Math.random() * 500),
+      description: todo,
+      isCompleted: false
+    }
+
+    try {
+      await api.post('/todos', {
+        ...updatedTodo
+      })
+      setTodos([...todos, updatedTodo])
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const handleIsCompletedChange = (todoId: Todo['id']) => {
+    const updatedTodos = todos.map((todo) => {
+      if (todo.id === todoId) {
+        return {
+          ...todo,
+          isCompleted: !todo.isCompleted
+        }
+      }
+      return todo
+    })
+    updateTodos(updatedTodos)
   }
 
   return (
-    <div className="bg-gray-300 h-screesn">
+    <div className="bg-gray-300 h-screen">
       <Header />
       <div className="max-w-3xl mx-auto px-1">
         <form
           onSubmit={handleSubmitNewTask}
           className="flex items-center gap-4 -mt-8"
         >
-          <Input />
+          <Input todo={todo} setTodo={setTodo} />
           <button
             type="submit"
             className="bg-[#4EA8DE] flex items-center gap-1 p-4 rounded-lg"
@@ -75,7 +107,11 @@ function App() {
               key={todo.id}
               className="flex items-center justify-between w-full gap-3 p-4 bg-gray-400 text-white mb-4 rounded-lg"
             >
-              <input type="checkbox" checked={todo.isCompleted} />
+              <input
+                type="checkbox"
+                checked={todo.isCompleted}
+                onChange={() => handleIsCompletedChange(todo.id)}
+              />
               <p>{todo.description}</p>
               <div className="flex items-center gap-4">
                 <AiOutlineEdit size={20} />
