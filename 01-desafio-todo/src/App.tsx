17f4f41aef071ebbba335f2b4bf5f1ca1a684214
Valdenício Ferreira <valdenicio22@gmail.com>
2022-06-09 20:47:07 -0300

    feat: create header component

--- 01-desafio-todo/src/App.tsx
+++ 01-desafio-todo/src/App.tsx
@@ -1,11 +1,14 @@
 import Input from './components/Input'
 import { ThemeProvider } from 'styled-components'
 import theme from './styles/theme'
+import { Header } from './components/Header'
+import { GlobalStyles } from './styles/global'
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Input />
+      <Header />
+      <GlobalStyles />
     </ThemeProvider>
   )
 }
