0c14d0e9c21526b6b096785b7d9c91f259a7c602
Valdenício Ferreira <valdenicio22@gmail.com>
2022-10-11 09:17:21 +0200

    feat: add theme

---
+++ React/Project-01/igniteFeed/src/styles/global.ts
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from 'styled-components'
+import theme from './theme'
+
+export const GlobalStyle = createGlobalStyle`
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  body {
+    background: ${theme.colors.gray900};
+    color: ${theme.colors.gray300};
+    -webkit-font-smoothing: antialiased;
+  }
+  body, input, textarea, button {
+    font-family: ${theme.font.family}, sans-serif;
+    font-weight: ${theme.font.weight.regular};
+    font-size: 1rem;
+  }
+  :focus {
+    outline: transparent;
+    box-shadow: 0 0 0 2px ${theme.colors.green500};
+  }
+`
---
+++ React/Project-01/igniteFeed/src/styles/theme.ts
@@ -0,0 +1,23 @@
+export default {
+  font: {
+    family: 'Roboto, sans-serif',
+    weight: {
+      regular: 400,
+      bold: 700
+    }
+  },
+  colors: {
+    gray100: '#e1e1e6',
+    gray300: '#c4c4cc',
+    gray400: '#8d8d99',
+    gray600: '#323238',
+    gray700: '#29292e',
+    gray800: '#202024',
+    gray900: '#121214',
+
+    green300: '#00B37E',
+    green500: ' #00875f',
+
+    red500: '#F75A68'
+  }
+} as const
